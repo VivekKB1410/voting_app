e883454f3b666d81b311b4afe6ddef06dce016b0
Vivekkb1410 <61176871+VivekKB1410@users.noreply.github.com>
2023-04-24 11:17:22 +0530

    Update loadbalancersexec.sh

--- loadbalancersexec.sh
+++ loadbalancersexec.sh
@@ -4,13 +4,13 @@ kubectl apply -f ballot/ballot.yaml -n $ROOST_NAMESPACE
 sleep 5
 
 ballot_loadbalancer=$(kubectl get svc -n $ROOST_NAMESPACE | grep ballot | awk '{print $4}')
-
+echo "ballot: $ballot_loadbalancer"
 kubectl apply -f ecserver/ecserver.yaml -n $ROOST_NAMESPACE
 
 sleep 5
 
 ecserver_loadbalancer=$(kubectl get svc -n $ROOST_NAMESPACE | grep ecserver | awk '{print $4}')
-
+echo "ecserver: $ecserver_loadbalancer"
 sed -i -e "s#REACT_APP_BALLOT_ENDPOINT_VALUE#${ballot_loadbalancer}:8080#" -e "s#REACT_APP_EC_SERVER_ENDPOINT_VALUE#${ecserver_loadbalancer}:8081#" voter/voter.yaml
 
 kubectl apply -f voter/voter.yaml -n $ROOST_NAMESPACE
